74f115da01ca8b30285e0ab541882c357be85ca7
Sanjay Chahal <sanjaychahal307@gmail.com>
2024-06-07 13:37:19 +0100

    First Commit
    FizzBuzz First commit

---
+++ Fizz-Buzz-Aviva/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Fizz_Buzz_Aviva.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
