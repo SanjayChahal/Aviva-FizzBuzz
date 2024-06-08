b214cd494481acdcaf9dc57b4caf63b8472938cd
Sanjay Chahal <sanjaychahal307@gmail.com>
2024-06-08 12:54:44 +0100

    Added pagination

--- FizzBuzzApp/Controllers/HomeController.cs
+++ FizzBuzzApp/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using FizzBuzzApp.Interfaces;
 using FizzBuzzApp.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace FizzBuzzApp.Controllers
 {
@@ -19,14 +20,40 @@ namespace FizzBuzzApp.Controllers
             ModelState.AddModelError("Number", "I got refreshed");
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int pageNumber = 1, int pageSize = 10)
         {
-            return View();
+            Console.WriteLine("I am on page number redirection");
+
+            // Retrieve the session data here
+            var sessionModel = HttpContext.Session.GetObject<List<FizzBuzzModel>>("FizzBuzzModel");
+
+            if (sessionModel != null)
+            {
+                _finalModel = sessionModel;
+            }
+
+            // Calculate the start index based on page number and page size
+            int startIndex = (pageNumber - 1) * pageSize;
+
+            // Get a subset of FizzBuzzModel based on pagination parameters
+            var modelSubset = _finalModel.Skip(startIndex).Take(pageSize).ToList();
+
+            // Calculate total number of pages
+            int totalPages = (int)Math.Ceiling((double)_finalModel.Count / pageSize);
+
+            // Set pagination related data in ViewBag
+            ViewBag.PageNumber = pageNumber;
+            ViewBag.TotalPages = totalPages;
+
+            // Pass the subset model to the view
+            return View(modelSubset);
         }
 
         [HttpPost]
         public IActionResult Index(int number)
         {
+            Console.WriteLine("IAction index ");
+
             if (number < 1 || number > 1000)
             {
                 ModelState.AddModelError("Number", "Please enter a number between 1 and 1000.");
@@ -50,12 +77,12 @@ namespace FizzBuzzApp.Controllers
             }
 
             HttpContext.Session.SetObject("FizzBuzzModel", _finalModel);
-            return View(_finalModel);
+            return RedirectToAction("Index");
         }
 
         private void InitializeFinalModel()
         {
-            // Check if _finalModel is null and initialize it if needed
+            // Ensure _finalModel is initialized
             _finalModel ??= new List<FizzBuzzModel>();
         }
     }
